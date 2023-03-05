fcaf4192e1d6fcd2c4e62368a4992582056041ca
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:08:33 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -6,3 +6,6 @@ from tkinter import Tk, Label, PhotoImage, messagebox
 class WeatherApp:
     def __init__(self,api_key):
         self.api_key = api_key
+        self.city_name = None
+        self.temperature = None
+        self.weather_description = None
