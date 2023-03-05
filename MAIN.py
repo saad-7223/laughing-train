5242e5f346beaa293540b0f44b27b41b572b3769
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:35:22 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -19,3 +19,10 @@ class WeatherApp:
         self.temperature = round(weather_data["main"]["temp"] - 273.15, 2)
         self.weather_description = weather_data["weather"][0]["description"].capitalize()
         self.weather_icon = weather_data["weather"][0]["icon"]
+    def get_forecast_data(self):
+        forecast_url = f"http://api.openweathermap.org/data/2.5/forecast?q={self.city_name}&appid={self.api_key}"
+        response = requests.get(forecast_url)
+        if response.status_code != 200:
+            raise ValueError("Failed to get forecast data")
+        forecast_data = json.loads(response.content)["list"]
+        self.forecast_data = []    
