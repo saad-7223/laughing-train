f4a3eefd32b25e13c29a7b76a958dc0b112aa0af
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:55:27 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -62,4 +62,9 @@ class WeatherApp:
         weather_label = Label(root, text=self.weather_description, font=("Arial", 20), bg="white")
         weather_label.pack()     
         forecast_label = Label(root, text="Forecast:", font=("Arial", 20), bg="white")
-        forecast_label.pack()      
+        forecast_label.pack() 
+        for forecast in self.forecast_data:
+            date_label = Label(root, text=forecast["date"], font=("Arial", 16), bg="white")
+            date_label.pack()
+            time_label = Label(root, text=forecast["time"], font=("Arial", 16), bg="white")
+                
