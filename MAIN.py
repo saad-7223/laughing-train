d899f818cfe5e1de585b2cc1fda020df5b1261d6
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:27:47 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -9,3 +9,11 @@ class WeatherApp:
         self.city_name = None
         self.temperature = None
         self.weather_description = None
+        self.forecast_data = None
+    def get_current_weather_data(self):
+        weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city_name}&appid={self.api_key}"
+        response = requests.get(weather_url)    
+        if response.status_code != 200:
+            raise ValueError("Failed to get weather data")
+        weather_data = json.loads(response.content)
+        
