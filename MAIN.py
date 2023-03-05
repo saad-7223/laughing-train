09c66ae42cf0066996ddf3cd143d42266362fc9d
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:53:36 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -60,4 +60,6 @@ class WeatherApp:
         temperature_label = Label(root, text=f"{self.temperature}°C", font=("Arial", 40), bg="white")
         temperature_label.pack()
         weather_label = Label(root, text=self.weather_description, font=("Arial", 20), bg="white")
-        weather_label.pack()           
+        weather_label.pack()     
+        forecast_label = Label(root, text="Forecast:", font=("Arial", 20), bg="white")
+        forecast_label.pack()      
