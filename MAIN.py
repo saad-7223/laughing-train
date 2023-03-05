8b22c4fcf3fb109c0c2be1b9ba4524c9bd482b63
saad-7223 <saadsyed837@gmail.com>
2023-03-05 16:01:56 +0530

    s

--- MAIN.py
+++ MAIN.py
@@ -3,5 +3,6 @@ import json
 from datetime import datetime, timedelta
 from tkinter import Tk, Label, PhotoImage, messagebox
 
-class WeatherApp: 
-    def __init__(self, api_key):
+class WeatherApp:
+    def __init__(self,api_key):
+        
